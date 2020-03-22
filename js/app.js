cf7d0263fe67cb36f382882d210ad70c0c87c815
Daniel diaz <danielcarcamomartinez@gmail.com>
2020-03-21 19:07:17 -0600

    js ajax

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", e=>{
       event.preventDefault();
 
      let nombre = document.querySelector("#txtNombre").value;
-     fetch(`saludo.php?name=${nombre}`)
+     fetch(`http//:80/saludo.php?name=${nombre}`)
           .then(resp=>resp.text())
           .then(respuesta=>{
               document.querySelector("#lblSaludo").innerHTML = respuesta;
